93f8a280f7315f40edbed9aca1499a1f04eea51c
Eduardo Peña <32526164+EPGamer@users.noreply.github.com>
2017-12-08 17:29:35 +0100

    Update primer_codigo_movimiento.ino

--- primer_codigo_movimiento.ino
+++ primer_codigo_movimiento.ino
@@ -1,13 +1,39 @@
+#include <Servo.h>
+
 int motor_left[] = {5, 6};
 int motor_right[] = {7, 8};
 int state = 0;
 int ledPin=12;
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+ 
+const int buzzerPin = 9;
+Servo servo;
 
 // --------------------------------------------------------------------------- Setup
 void setup() {
 Serial.begin(9600);
     pinMode(ledPin, OUTPUT);   //Declara pin de Salida
     digitalWrite(ledPin, LOW); //Normalmente Apagado
+    pinMode(buzzerPin, OUTPUT);
+  
 
 // Setup motors
 int i;
@@ -15,6 +41,11 @@ for(i = 0; i < 2; i++){
 pinMode(motor_left[i], OUTPUT);
 pinMode(motor_right[i], OUTPUT);
 }
+
+//Setup Servo
+
+ servo.attach(10);
+  
 
 }
 
@@ -27,18 +58,37 @@ void loop() {
  
  // si el estado es 0 ese sería Apagado “OFF”
  if (state == '0') {
-          digitalWrite(ledPin, HIGH);
+          digitalWrite(ledPin, LOW);
     drive_forward();
-    Serial.println("LED: off");
  }
  
  // de lo contrario si el estado es 1 ese sería Encendido “ON”
- else
  if (state == '1') {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(ledPin, HIGH);
 drive_backward();
-Serial.println("LED: on");
  }
+  if (state == '2') {
+turn_left();
+ }
+  if (state == '3') {
+turn_right();
+ }
+  if (state == '4') {
+motor_stop();
+ }
+
+ if (state == '5') {
+firstSection();
+ }
+ if (state == '6') {
+servo1();
+ }
+ if (state == '7') {
+servo2();
+ }
+
+ 
+ state=-1;
 }
 
 // --------------------------------------------------------------------------- Drive
@@ -53,20 +103,23 @@ delay(25);
 }
 
 void drive_forward(){
-digitalWrite(motor_left[0], HIGH); 
-digitalWrite(motor_left[1], LOW); 
-
-digitalWrite(motor_right[0], HIGH); 
-digitalWrite(motor_right[1], LOW); 
-}
-
-void drive_backward(){
 digitalWrite(motor_left[0], LOW); 
 digitalWrite(motor_left[1], HIGH); 
 
 digitalWrite(motor_right[0], LOW); 
 digitalWrite(motor_right[1], HIGH); 
 }
+
+void drive_backward(){
+  
+digitalWrite(motor_left[0], HIGH); 
+digitalWrite(motor_left[1], LOW); 
+
+digitalWrite(motor_right[0], HIGH); 
+digitalWrite(motor_right[1], LOW); 
+
+}
+
 
 void turn_left(){
 digitalWrite(motor_left[0], LOW); 
@@ -84,4 +137,59 @@ digitalWrite(motor_right[0], LOW);
 digitalWrite(motor_right[1], HIGH); 
 }
 
+// --------------------------------------------------------------------------- Buzzer
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note, duration);
+   delay(duration);
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+  delay(50);
+  
+}
+ 
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+ 
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+  state=-1;
+}
+ 
 
+// --------------------------------------------------------------------------- Fire
+
+
+void servo1(){
+      
+      servo.write(105);
+      
+    } 
+
+    void servo2(){
+      
+      servo.write(0);
+      
+    } 
+  
